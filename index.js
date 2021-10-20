9127f81a04b0a58019c71f7e34173e63beb17add
Lisa Søndergaard <lisa3074@stud.kea.dk>
2021-10-20 11:56:24 +0200

    doing blogpost app

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_PORT, MONGO_IP } = require("./config/config.js");
+
+const postRouter = require("./routes/postRoutes");
 
 const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    })
+    .then(() => console.log("Sucssesfully connected to db"))
+    .catch(e => {
+      console.log("Theres an error", e);
+      //Make sure the app tries to reconnect to db, if db isnt up (indefinate)
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
-  res.send("<h2>Hello</h2>");
+  res.send("<h2>Hell yeah!! Wuhuuu</h2>");
 });
+
+app.use("/posts", postRouter);
 
 //This points to the express port that has been set (PORT), if it hasn't been set, fall back to the default port we specified as 3000
 const port = process.env.PORT || 8000;
